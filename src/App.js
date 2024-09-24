29a9b3535c20cf560e2dd69522e2538433861106
dev-jane99 <ruejaeeun99@gmail.com>
2024-09-24 17:43:38 +0900

    day02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react"
-import Color from "./day01/state/Color";
+import Checkbox from "./day02/ref/expert/Checkbox";
+// import FoodContainer from "./day02/ref/expert/FoodContainer";
+// import Check from "./day02/ref/basic/Check";
+// import Name from "./day02/ref/basic/Name";
+// import Color from "./day01/state/Color";
+// import FoodContainer from "./day02/map/basic/FoodContainer";
+// import UserContainer from "./day02/map/normal/UserContainer";
 // import Name from "./day01/state/Name";
 // import Sound from "./day01/state/Sound";
 // import Count from "./day01/state/Count";
@@ -12,7 +18,13 @@ function App() {
     {/* <Count /> */}
     {/* <Sound /> */}
     {/* <Name /> */}
-    <Color />
+    {/* <Color /> */}
+    {/* <FoodContainer /> */}
+    {/* <UserContainer /> */}
+    {/* <Name /> */}
+    {/* <Check /> */}
+    {/* <FoodContainer /> */}
+    <Checkbox />
     </>
   );
 }
---
+++ src/day02/map/normal/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = ({city}) => {
+  return (
+    <li>
+      Name of the users city : {city}
+    </li>
+  );
+};
+
+export default User;
